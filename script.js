4f0b2e1959d6660bd16c921be6b6e93173855ced
= <pkaramagi1@gmail.com>
2021-07-23 11:36:02 +0000

    Modified Javascript to Add functionality to UI

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+window.addEventListener('load', function () {
+  
+  var rate = document.getElementById("rate");
+var output = document.getElementById("interest-value");
+output.innerHTML = rate.value + "%"; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+rate.oninput = function() {
+	
+  output.innerHTML = (this.value) + "%";
+}
+  
+});
+
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+	var output;
+	var rate = document.getElementById("rate").value;
+	var principal= document.getElementById("principal").value;
+	var years = document.getElementById("years").value;
+	var resultContainer = document.getElementById("result");
+	
+	if(isNaN(principal)){
+		alert('Please input numbers only');
+	}
+	else{
+		yearlyInterest = principal * (rate/100);
+		totalInterest = yearlyInterest * years;
+		formattedYears = new Date().getFullYear() + Number(years);
+		output = 'If you deposit '+principal+',<br/>';
+		output += 'at an interest of '+rate+'%.<br/>';
+		output += 'You will recieve an amount of ' +totalInterest+',<br/>';
+		output += 'in the year '+formattedYears; 
+	
+
+		resultContainer.innerHTML = output;
+		
+	}
+		
+	
     
 }
-        
+    
+
+    
